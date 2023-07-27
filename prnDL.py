81374f3ca3e5dbf8ffbcf71f26b67e436b95cd1b
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2023-07-27 14:40:41 +0200

    Update prnDL.py
    fixed typo

--- prnDL.py
+++ prnDL.py
@@ -6,7 +6,7 @@ prodid = url.split('/')[4]
 
 with open('cookies.txt', 'r') as f: bearer_token  = f.readline()
 headers = {'x-authorization': bearer_token}
-response = requests.get('https://etext-ise.pearson.com/marin/api/1.0/products/99695/token', params={'setCookie': 'true'}, headers={'authorization': f'Bearer {bearer_token}'})
+response = requests.get(f'https://etext-ise.pearson.com/marin/api/1.0/products/{prodid}/token', params={'setCookie': 'true'}, headers={'authorization': f'Bearer {bearer_token}'})
 params = {'productId': response.json()["value"]}
 
 class Metadata:
